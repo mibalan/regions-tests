71709fb7137deef36020075bd80b96e795f373b3
Mihai Balan <mibalan@adobe.com>
2012-05-10 18:31:12 +0300

    Added PrefixFree lib. Still have one test to migrate to it

--- testregions.js
+++ testregions.js
@@ -1,10 +1,20 @@
 $(function () {
+    function prefixOM(name) {
+        return PrefixFree.Prefix.toLowerCase() + name.replace(/^[a-z]/, function($0){ return $0.toUpperCase(); });
+    }
+    function prefixMethod(obj, method) {
+        if (obj[method]) {
+            return obj[method].bind(obj);
+        } else {
+            return obj[prefixOM(method)].bind(obj);
+        }
+    }
     
     var $flow, $region;  
     
     function setup(){
-        $flow = $('<div />').css("-webkit-flow-into", "article");    
-        $region = $('<div />').css("-webkit-flow-from", "article"); 
+        $flow = $('<div />').css("flow-into", "article");
+        $region = $('<div />').css("flow-from", "article"); 
             
         $("body").append($flow, $region)    
     }   
@@ -19,7 +29,7 @@ $(function () {
     test("Named flow - content should be pulled to a flow", function(){ 
         setup();
         
-        ok($flow.css("-webkit-flow-into") == "article");
+        ok($flow.css("flow-into") == "article");
         
         teardown();
     })
@@ -27,7 +37,7 @@ $(function () {
     test("Region - should consume from flow", function(){
         setup();
 
-        ok($region.css("-webkit-flow-from") == "article");
+        ok($region.css("flow-from") == "article");
 
         teardown();
     })
@@ -35,10 +45,10 @@ $(function () {
     test("Region properties - region-overflow property", function() {
         setup();
         
-        equal($region.css('-webkit-region-overflow'), 'auto', 'Initial default value for region-overflow');
+        equal($region.css('region-overflow'), 'auto', 'Initial default value for region-overflow');
         
-        $region.css('-webkit-region-overflow', 'break');
-        equal($region.css('-webkit-region-overflow'), 'break', 'region-overflow: break');
+        $region.css('region-overflow', 'break');
+        equal($region.css('region-overflow'), 'break', 'region-overflow: break');
 
         teardown();
     })
@@ -50,8 +60,8 @@ $(function () {
     
     test("Document should return a flow by name", function(){      
         setup(); 
-        
-        ok(document.webkitGetFlowByName("article"));
+
+        ok(prefixMethod(document, "getFlowByName")("article"));
         
         teardown();
     }) 
@@ -59,7 +69,7 @@ $(function () {
     test("NamedFlow should have overflow property", function(){ 
         setup();
 
-        ok(document.webkitGetFlowByName("article").overflow === false)   
+        ok(prefixMethod(document, "getFlowByName")("article").overflow === false);
 
         teardown(); 
     })
@@ -67,7 +77,7 @@ $(function () {
     test("NamedFlow should have contentNodes property", function() {
         setup();
 
-        var namedFlow = document.webkitGetFlowByName("article");
+        var namedFlow = prefixMethod(document, "getFlowByName")("article");
         ok(namedFlow.contentNodes, "NamedFlow.contentNodes");
         equal(namedFlow.contentNodes.length, 1, "NamedFlow.contentNodes has one node")
 
@@ -77,7 +87,7 @@ $(function () {
     test("NamedFlow should have getRegionsByContentNode() function", function() {
         setup();
 
-        var namedFlow = document.webkitGetFlowByName("article");
+        var namedFlow = prefixMethod(document, "getFlowByName")("article");
         equal(typeof(namedFlow.getRegionsByContentNode), "function", "NamedFlow.getRegionsByContentNode is a function");
 
         teardown();
@@ -86,7 +96,7 @@ $(function () {
         setup();
 
         $flow.html('Foo');
-        var namedFlow = document.webkitGetFlowByName("article");
+        var namedFlow = prefixMethod(document, "getFlowByName")("article");
         var theRegions = namedFlow.getRegionsByContentNode($flow.contents()[0]);
 
         equal(theRegions.length, 1, "One region for the content");
@@ -108,15 +118,15 @@ $(function () {
         
         // lots of content, expect overflow
         $flow.html("Long text Long text Long text Long text ");
-        ok($region[0].webkitRegionOverflow == "overflow"); 
+        ok($region[0][prefixOM("regionOverflow")] == "overflow"); 
 
         // less content, expect fit
         $flow.html("x");
-        ok($region[0].webkitRegionOverflow == "fit"); 
+        ok($region[0][prefixOM("regionOverflow")] == "fit"); 
         
         // no content, expect empty
         $flow.html("");
-        ok($region[0].webkitRegionOverflow == "empty"); 
+        ok($region[0][prefixOM("regionOverflow")] == "empty"); 
 
         teardown(); 
     })
@@ -125,7 +135,7 @@ $(function () {
 
     asyncTest("regionLayoutUpdate event is thrown", function(){
         function handler(ev) {
-            $region.unbind("webkitRegionLayoutUpdate", handler);
+            $region.unbind(prefixOM("regionLayoutUpdate"), handler);
             equal(ev.target, $region[0], "Event.target points to the region");
             teardown();
             start();
@@ -139,13 +149,14 @@ $(function () {
             }
         );
         $flow.html("M");
-        $region.bind("webkitRegionLayoutUpdate", handler);
+        $region.bind(prefixOM("regionLayoutUpdate"), handler);
         $flow.html("Long text long text long text long long long longer very longer text");
     })
 
     module("Region styling");
 
     test("Basic @region rule support", function() {
+        //FIXME: Still need a way to determine this in a prefix-free way
         ok(window.WebKitCSSRegionRule, "@region rules seem to be supported");
     });
 })   
